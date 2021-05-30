bb26d50698b8cfd28037c357c36b44e096d060e9
Lyuma <xn.lyuma@gmail.com>
2021-05-30 00:02:18 -0700

    Version 1.1.0 - Substantial reorganization in preparation of AssetLib release; added Godette sample VRM model.

--- vrm_samples/sample_script.gd
+++ vrm_samples/sample_script.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$"Godette_vrm_v4/anim".play("wiggle3")
 	$"AliciaSolid_vrm-051/anim".play("wiggle4")
 	$"AliciaSolid_vrm-052/anim".play("wiggle")
 	return # Replace with function body.
