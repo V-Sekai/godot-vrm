599e376686db7a4c44982505f88acec6281bed3d
Lyuma <xn.lyuma@gmail.com>
2022-01-05 16:05:30 -0800

    Implement new GLTFDocument.append_from_buffer API and add options support

--- addons/vrm/import_vrm.gd
+++ addons/vrm/import_vrm.gd
@@ -18,14 +18,14 @@ func _get_import_flags() -> int:
 	return IMPORT_SCENE
 
 
-func _import_animation(path: String, flags: int, bake_fps: int) -> Animation:
+func _import_animation(path: String, flags: int, options: Dictionary, bake_fps: int) -> Animation:
 	return Animation.new()
 
 
-func _import_scene(path: String, flags: int, bake_fps: int) -> Node:
+func _import_scene(path: String, flags: int, options: Dictionary, bake_fps: int) -> Node:
 	var vrm_loader = load("res://addons/vrm/vrm_loader.gd").new()
 	
-	var root_node = vrm_loader.import_scene(path, flags, bake_fps, true)
+	var root_node = vrm_loader.import_scene(path, flags, bake_fps)
 	
 	if typeof(root_node) == TYPE_INT:
 		return root_node # Error code
