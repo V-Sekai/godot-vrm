5aeb54adf3df4ebe81db9d69249885785474015b
Lyuma <xn.lyuma@gmail.com>
2022-05-27 01:45:52 -0700

    Re-add sample animation as a separate library.

--- vrm_samples/sample_script.gd
+++ vrm_samples/sample_script.gd
@@ -2,5 +2,6 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"Godette_vrm_v4/anim".play("wiggle3")
-	$"AliciaSolid_vrm-051/anim".play("wiggle4")
+	$"Godette_vrm_v4/anim".play("sample/wiggle4")
+	$"AliciaSolid_vrm-051/anim".play("sample/wiggle4")
+	$"AliciaSolid_vrm-052/anim".play("sample/wiggle4")
