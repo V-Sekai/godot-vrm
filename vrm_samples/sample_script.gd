f609e3490298103f94a70fe1fbc379599d48f6e0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-25 16:05:28 -0800

    Restore the demo loop.

--- vrm_samples/sample_script.gd
+++ vrm_samples/sample_script.gd
@@ -2,6 +2,12 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var wiggle4 : Animation = $"Godette_vrm_v4/anim".get_animation("sample/wiggle4")
+	wiggle4.loop_mode = Animation.LOOP_LINEAR
 	$"Godette_vrm_v4/anim".play("sample/wiggle4")
+	wiggle4 = $"AliciaSolid_vrm-051/anim".get_animation("sample/wiggle4")
+	wiggle4.loop_mode = Animation.LOOP_LINEAR
 	$"AliciaSolid_vrm-051/anim".play("sample/wiggle4")
+	wiggle4 = $"AliciaSolid_vrm-052/anim".get_animation("sample/wiggle4")
+	wiggle4.loop_mode = Animation.LOOP_LINEAR
 	$"AliciaSolid_vrm-052/anim".play("sample/wiggle4")
