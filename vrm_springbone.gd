8591cf11f8dc0dad6d1108aa9a4ac35c22fc137d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-25 21:23:18 -0800

    Rename spring bone names to be consistent with godot.

--- vrm_springbone.gd
+++ vrm_springbone.gd
@@ -106,17 +106,39 @@ class VRMSpringBoneLogic:
 
 	var initial_transform: Transform3D
 
+	static func global_pose_to_local_pose(p_skeleton: Skeleton3D, p_bone_idx: int,  p_global_pose: Transform3D) -> Transform3D:
+		var bone_size : int = p_skeleton.get_bone_count()
+		if p_bone_idx < 0 or p_bone_idx >= bone_size:
+			return Transform3D()
+		if p_skeleton.get_bone_parent(p_bone_idx) >= 0:
+			var parent_bone_idx : int = p_skeleton.get_bone_parent(p_bone_idx)
+			var conversion_transform : Transform3D = p_skeleton.get_bone_global_pose(parent_bone_idx).affine_inverse()
+			return conversion_transform * p_global_pose
+		else:
+			return p_global_pose
+			
+	static func local_pose_to_global_pose(p_skeleton : Skeleton3D, p_bone_idx: int, p_local_pose: Transform3D) -> Transform3D:
+		var bone_size : int = p_skeleton.get_bone_count()
+		if p_bone_idx < 0 or p_bone_idx >= bone_size:
+			return Transform3D()
+		if p_skeleton.get_bone_parent(p_bone_idx) >= 0:
+			var parent_bone_idx : int = p_skeleton.get_bone_parent(p_bone_idx)
+			return p_skeleton.get_bone_global_pose(parent_bone_idx) * p_local_pose;
+		else:
+			return p_local_pose
+
+
 	func get_transform(skel: Skeleton3D) -> Transform3D:
 		return skel.get_global_transform() * skel.get_bone_global_pose_no_override(bone_idx)
 
-	func get_rotation(skel: Skeleton3D) -> Quaternion:
+	func get_rotation_relative_to_origin(skel: Skeleton3D) -> Quaternion:
 		return get_transform(skel).basis.get_rotation_quaternion()
 
-	func get_local_transform(skel: Skeleton3D) -> Transform3D:
+	func get_global_pose(skel: Skeleton3D) -> Transform3D:
 		return skel.get_bone_global_pose_no_override(bone_idx)
 
-	func get_local_rotation(skel: Skeleton3D) -> Quaternion:
-		return get_local_transform(skel).basis.get_rotation_quaternion()
+	func get_local_pose_rotation(skel: Skeleton3D) -> Quaternion:
+		return get_global_pose(skel).basis.get_rotation_quaternion()
 
 	func reset(skel: Skeleton3D) -> void:
 		skel.set_bone_global_pose_override(bone_idx, initial_transform, 1.0, true)
@@ -144,7 +166,7 @@ class VRMSpringBoneLogic:
 			tmp_prev_tail = prev_tail
 
 		# Integration of velocity verlet
-		var next_tail: Vector3 = tmp_current_tail + (tmp_current_tail - tmp_prev_tail) * (1.0 - drag_force) + (get_rotation(skel) * (bone_axis)) * stiffness_force + external
+		var next_tail: Vector3 = tmp_current_tail + (tmp_current_tail - tmp_prev_tail) * (1.0 - drag_force) + (get_rotation_relative_to_origin(skel) * (bone_axis)) * stiffness_force + external
 
 		# Limiting bone length
 		var origin: Vector3 = get_transform(skel).origin
@@ -162,13 +184,13 @@ class VRMSpringBoneLogic:
 			current_tail = next_tail
 
 		# Apply rotation
-		var ft = VRMTopLevel.VRMUtil.from_to_rotation(get_rotation(skel) * (bone_axis), next_tail - get_transform(skel).origin)
+		var ft = VRMTopLevel.VRMUtil.from_to_rotation(get_rotation_relative_to_origin(skel) * (bone_axis), next_tail - get_transform(skel).origin)
 		if typeof(ft) != TYPE_NIL:
 			ft = skel.global_transform.basis.get_rotation_quaternion().inverse() * ft
-			var qt: Quaternion = ft * get_rotation(skel)
-			var local_tr: Transform3D = get_local_transform(skel)
-			local_tr.basis = Basis(qt.normalized())
-			skel.set_bone_global_pose_override(bone_idx, local_tr, 1.0, true)
+			var qt: Quaternion = ft * get_rotation_relative_to_origin(skel)
+			var global_pose_tr: Transform3D = get_global_pose(skel)
+			global_pose_tr.basis = Basis(qt.normalized())
+			skel.set_bone_global_pose_override(bone_idx, global_pose_tr, 1.0, true)
 
 	func collision(skel: Skeleton3D, colliders: Array, _next_tail: Vector3) -> Vector3:
 		var out: Vector3 = _next_tail
