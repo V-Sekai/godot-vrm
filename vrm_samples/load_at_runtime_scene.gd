30486f69eeeb3cc345c6e890b3e4b0a47d1c41be
Tim Yuen <timothy.yuen22@gmail.com>
2023-05-28 01:53:28 -0400

    add files_dropped functionality

--- vrm_samples/load_at_runtime_scene.gd
+++ vrm_samples/load_at_runtime_scene.gd
@@ -1,11 +1,34 @@
 extends Node3D
 
+var _model: Node3D = null
+
 func _ready():
-	var model: Node3D = _load_model("res://vrm_samples/AliciaSolid_vrm-0.51.vrm")
+	get_tree().root.files_dropped.connect(func(files: PackedStringArray) -> void:
+		if files.size() != 1:
+			printerr("This demo only handles loading 1 model at a time.")
+			return
+		
+		var file_path := files[0]
+		if not file_path.get_extension().to_lower() in ["vrm", "glb"]:
+			printerr("This demo only handles vrm and glb files.")
+			return
+		
+		var model: Node3D = _load_model(file_path)
+		if model == null:
+			printerr("Failed to load model from path %s" % file_path)
+			return
+		
+		if _model != null:
+			_model.free()
+		
+		_model = model
+		add_child(model)
+	)
+	
+	_model = _load_model("res://vrm_samples/AliciaSolid_vrm-0.51.vrm")
+	add_child(_model)
 
-	add_child(model)
-
-func _load_model(path: String) -> Node:
+func _load_model(path: String) -> Node3D:
 	var gltf: GLTFDocument = GLTFDocument.new()
 	var vrm_extension: GLTFDocumentExtension = preload("res://addons/vrm/vrm_extension.gd").new()
 	gltf.register_gltf_document_extension(vrm_extension, true)
