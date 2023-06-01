5f043ad619451a2437f9b0b5bfd48301a9d085bb
Aaron Franke <arnfranke@yahoo.com>
2023-06-01 00:24:19 -0500

    Add VRMC_node_constraint

--- addons/vrm/plugin.gd
+++ addons/vrm/plugin.gd
@@ -7,6 +7,9 @@ var import_plugin
 func _enter_tree() -> void:
 	# NOTE: Be sure to also register at runtime if you want runtime import.
 	# This editor plugin script won't run outside of the editor.
+	var ext: GLTFDocumentExtension
+	ext = GLTFDocumentExtensionVRMNodeConstraint.new()
+	GLTFDocument.register_gltf_document_extension(ext)
 	import_plugin = preload("res://addons/vrm/import_vrm.gd").new()
 	add_scene_format_importer_plugin(import_plugin)
 
