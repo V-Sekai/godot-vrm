ecdb341e30d50ba82441f94eff40c4e27649544e
Aaron Franke <arnfranke@yahoo.com>
2023-06-03 18:34:17 -0500

    Update Git metadata and update project name

--- addons/vrm/plugin.gd
+++ addons/vrm/plugin.gd
@@ -4,11 +4,13 @@ extends EditorPlugin
 var import_plugin
 
 
-func _enter_tree():
+func _enter_tree() -> void:
+	# NOTE: Be sure to also register at runtime if you want runtime import.
+	# This editor plugin script won't run outside of the editor.
 	import_plugin = preload("res://addons/vrm/import_vrm.gd").new()
 	add_scene_format_importer_plugin(import_plugin)
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_scene_format_importer_plugin(import_plugin)
 	import_plugin = null
