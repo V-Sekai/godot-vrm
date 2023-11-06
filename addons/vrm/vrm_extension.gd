11d41783e9e3dd9da99e613f0beb9f1c24766281
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2023-11-06 08:32:50 -0800

    Handle missing bone groups and collider groups

--- addons/vrm/vrm_extension.gd
+++ addons/vrm/vrm_extension.gd
@@ -890,13 +890,15 @@ func _add_vrm_nodes_to_skin(obj: Dictionary) -> bool:
 	var new_joints_set = {}.duplicate()
 
 	var secondaryAnimation = vrm_extension.get("secondaryAnimation", {})
-	for bone_group in secondaryAnimation.get("boneGroups", []):
-		for bone in bone_group["bones"]:
-			_add_joints_recursive(new_joints_set, obj["nodes"], int(bone), true)
+	for bone_group in secondaryAnimation.get("boneGroups", {}):
+		if bone_group.has("bones"):
+			for bone in bone_group["bones"]:
+				_add_joints_recursive(new_joints_set, obj["nodes"], int(bone), true)
 
-	for collider_group in secondaryAnimation.get("colliderGroups", []):
-		if int(collider_group["node"]) >= 0:
-			new_joints_set[int(collider_group["node"])] = true
+	for collider_group in secondaryAnimation.get("colliderGroups", {}):
+		if collider_group.has("node"):
+			if int(collider_group["node"]) >= 0:
+				new_joints_set[int(collider_group["node"])] = true
 
 	var firstPerson = vrm_extension.get("firstPerson", {})
 	if firstPerson.get("firstPersonBone", -1) >= 0:
